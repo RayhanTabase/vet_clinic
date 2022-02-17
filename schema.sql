9a8b4d48ae13f7c13d864b8fa8406219f144cc20
Rayhan <maouslayer@gmail.com>
2022-02-17 14:08:16 +0000

    Update SQL

--- schema.sql
+++ schema.sql
@@ -12,4 +12,27 @@ CREATE TABLE animals (
 ALTER TABLE animals
 ADD species varchar(200);
 
+-- Create a table named owners with the following columns
+CREATE TABLE owners (
+  id INT PRIMARY KEY NOT NULL GENERATED ALWAYS AS IDENTITY,
+  full_name VARCHAR(500) NOT NULL,
+  age INT NOT NULL
+);
+
+-- Create a table named species with the following columns
+CREATE TABLE species (
+  id INT PRIMARY KEY NOT NULL GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(200) NOT NULL
+);
+
+-- Modify animals table
+ALTER TABLE animals
+DROP COLUMN species;
+
+ALTER TABLE animals
+ADD species_id INT REFERENCES species(id) ON DELETE SET NULL;
+
+ALTER TABLE animals
+ADD owner_id INT REFERENCES owners(id) ON DELETE SET NULL;
+
 
